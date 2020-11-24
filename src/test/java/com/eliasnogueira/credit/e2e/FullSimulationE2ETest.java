0d669d043037439635649b34efaa285f97183340
elias.nogueira@gmail.com <9365657321ea49f8473753a689d824727b8b1aec>
2020-11-24 08:14:15 +0100

    assertion in the client return

--- src/test/java/com/eliasnogueira/credit/e2e/FullSimulationE2ETest.java
+++ src/test/java/com/eliasnogueira/credit/e2e/FullSimulationE2ETest.java
@@ -23,21 +23,38 @@
  */
 package com.eliasnogueira.credit.e2e;
 
-import static com.eliasnogueira.credit.data.suite.TestTags.E2E;
-
 import com.eliasnogueira.credit.client.RestrictionsClient;
 import com.eliasnogueira.credit.client.SimulationsClient;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.eliasnogueira.credit.data.suite.TestTags.E2E;
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FullSimulationE2ETest {
+
+    private static RestrictionsClient restrictionsClient;
+    private static SimulationsClient simulationsClient;
+
+    @BeforeAll
+    static void beforeE2e() {
+        restrictionsClient = new RestrictionsClient();
+        simulationsClient = new SimulationsClient();
+    }
+
 
     @Test
     @Tag(E2E)
     @DisplayName("Should submit a successful simulation for a non-restricted CPF")
     void completeSimulation() {
-        new RestrictionsClient().queryRestrictionAndReturnNotFound();
-        new SimulationsClient().submitSuccessfulSimulation();
+        Response notFound = restrictionsClient.queryRestrictionAndReturnNotFound();
+        assertThat(notFound.getStatusCode()).isEqualTo(SC_NOT_FOUND);
+
+        Response successfulSimulation = simulationsClient.submitSuccessfulSimulation();
+        assertThat(successfulSimulation.getHeader("Location")).isNotEmpty();
     }
 }
