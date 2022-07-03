2ae94ac63bbd84539f74ef506fdc1e46c72039a5
Elias Nogueira <elias.nogueira@gmail.com>
2022-07-03 12:12:56 +0200

    reformat code and moved instants to field

--- src/test/java/com/eliasnogueira/credit/e2e/FullSimulationE2ETest.java
+++ src/test/java/com/eliasnogueira/credit/e2e/FullSimulationE2ETest.java
@@ -25,36 +25,27 @@ package com.eliasnogueira.credit.e2e;
 
 import com.eliasnogueira.credit.client.RestrictionsClient;
 import com.eliasnogueira.credit.client.SimulationsClient;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.eliasnogueira.credit.data.suite.TestTags.E2E;
+import static com.eliasnogueira.credit.data.changeless.TestSuiteTags.E2E;
 import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FullSimulationE2ETest {
 
-    private static RestrictionsClient restrictionsClient;
-    private static SimulationsClient simulationsClient;
-
-    @BeforeAll
-    static void beforeE2e() {
-        restrictionsClient = new RestrictionsClient();
-        simulationsClient = new SimulationsClient();
-    }
-
+    private final RestrictionsClient restrictionsClient = new RestrictionsClient();
+    private final SimulationsClient simulationsClient = new SimulationsClient();
 
     @Test
     @Tag(E2E)
     @DisplayName("Should submit a successful simulation for a non-restricted CPF")
     void completeSimulation() {
-        Response notFound = restrictionsClient.queryRestrictionAndReturnNotFound();
+        var notFound = restrictionsClient.queryRestrictionAndReturnNotFound();
         assertThat(notFound.getStatusCode()).isEqualTo(SC_NOT_FOUND);
 
-        Response successfulSimulation = simulationsClient.submitSuccessfulSimulation();
+        var successfulSimulation = simulationsClient.submitSuccessfulSimulation();
         assertThat(successfulSimulation.getHeader("Location")).isNotEmpty();
     }
 }
