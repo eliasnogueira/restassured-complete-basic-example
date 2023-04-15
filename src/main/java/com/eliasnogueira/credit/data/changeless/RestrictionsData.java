f5f64bfb0931fcdec1bc1079a68640f365d95e11
Elias Nogueira <elias.nogueira@gmail.com>
2023-04-15 11:56:01 +0200

    fix restrictions endpoint

--- src/main/java/com/eliasnogueira/credit/data/changeless/RestrictionsData.java
+++ src/main/java/com/eliasnogueira/credit/data/changeless/RestrictionsData.java
@@ -28,7 +28,7 @@ public final class RestrictionsData {
     private RestrictionsData() {
     }
 
-    public static final String SERVICE = "/restrictions/";
+    public static final String SERVICE = "/restrictions";
     public static final String GET_RESTRICTIONS = String.format("%s/{cpf}", SERVICE);
     public static final String CPF = "cpf";
 }
