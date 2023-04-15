43a3ef8d9b36da0c75ec2f291d9d1683d31be31a
Elias Nogueira <elias.nogueira@gmail.com>
2023-04-15 11:52:20 +0200

    removal if cpf check

--- src/main/java/com/eliasnogueira/credit/data/changeless/SimulationErrorsData.java
+++ src/main/java/com/eliasnogueira/credit/data/changeless/SimulationErrorsData.java
@@ -29,7 +29,7 @@ public enum SimulationErrorsData {
     ERRORS_AMOUNT_LESS("errors.amount", "Amount must be equal or less than than $ 40.000"),
     ERRORS_INSTALLMENTS_GREATER("errors.installments", "Installments must be equal or greater than 2"),
     ERRORS_INSTALLMENTS_LESS("errors.installments", "Installments must be equal or less than 48"),
-    ERRORS_EMAIL("errors.email", "must be a well-formed email address"),
+    ERRORS_EMAIL("errors.email", "E-mail must be valid"),
     ERRORS_CPF("errors.cpf", "CPF cannot be empty"),
     ERRORS_NAME("errors.name", "Name cannot be empty");
 
